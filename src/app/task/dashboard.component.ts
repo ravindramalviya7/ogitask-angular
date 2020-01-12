f70dc263e82efc12db1a9aa0041deb08bf740e80
Shivam Rathod <shivam.rathod@divergentsl.com>
2020-01-12 22:26:11 +0530

    Shivam: Side bar

--- src/app/task/dashboard.component.ts
+++ src/app/task/dashboard.component.ts
@@ -1,12 +1,33 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import '../../assets/js/material-kit.js?v=2.0.6';
 
+declare var $;
 @Component({
-  selector: 'dashboard-root',
+  selector: 'app-dashboard-root',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   title = 'ogitasks';
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    $('#sidebar').mCustomScrollbar({
+      theme: 'minimal'
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+      // open or close navbar
+      $('#sidebar').toggleClass('active');
+      $('#content').toggleClass('active');
+      // close dropdowns
+      $('.collapse.in').toggleClass('in');
+      // and also adjust aria-expanded attributes we use for the open/closed arrows
+      // in our CSS
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+  }
 }
